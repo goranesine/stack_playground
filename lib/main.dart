aefabe3cd64671712731b2c64c49dcde01c3f2af
Goran <goranesine@gmail.com>
2019-08-04 17:38:48 +0200

    Kartice se okrecu

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Okreni kartice',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Card Stack'),
+      home: MyHomePage(title: 'Okreni kartice'),
     );
   }
 }
@@ -25,66 +25,216 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
-  @override
+  bool _visible = true;
+
+
+   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-              top: 10,
-              child: Card(
-                elevation: 4,
-                color: Color.fromARGB(255, 0, 0, 255),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  width: 200,
-                  height: 300,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 20,
-              child: Card(
-                elevation: 8,
-                color: Color.fromARGB(255, 0, 255, 0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  width: 220,
-                  height: 300,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 30,
-              child: Card(
-                elevation: 12,
-                color: Color.fromARGB(255, 200, 0, 0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  width: 240,
-                  height: 300,
-                ),
-              ),
-            )
-          ],
-        ),
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: <Widget>[
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+                   AnimatedOpacity(
+                     opacity: !_visible ? 1.0 : 0.0,
+                     duration: Duration(milliseconds: 500),
+
+
+                     child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.green,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.red,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: !_visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.pink,
+                 ),
+               ),
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.orange,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: !_visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.blue,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.red,
+                 ),
+               ),
+
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               AnimatedOpacity(
+                 opacity: !_visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.amber,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.white30,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: !_visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.deepOrange,
+                 ),
+               ),
+
+
+
+
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.lightBlue,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: !_visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.lightGreenAccent,
+                 ),
+               ),
+               AnimatedOpacity(
+                 opacity: _visible ? 1.0 : 0.0,
+                 duration: Duration(milliseconds: 500),
+
+
+                 child: Container(
+
+                   width: 100,
+                   height: 100,
+                   color: Colors.purple,
+                 ),
+               ),
+
+
+             ],
+           ),
+         ],
+       ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+          pokreniAnimaciju();
+        },
+        tooltip: 'Toggle Opacity',
+        child: Icon(Icons.flip),
+      ),
+
     );
   }
-}
+
+    bool pokreniAnimaciju(){
+      _visible = !_visible;
+      setState(() {
+
+      });
+      return _visible;
+
+    }
+
+
+  }
